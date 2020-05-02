e56368fe182689ff5d8acfc25b7cdebff3eafa15
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-01 22:44:43 -0400

    UI added

---
+++ app.js
@@ -0,0 +1,10 @@
+const slots = ['first','second','third'];
+
+const users =[
+    {id: 1 ,name: 'moe', slot: 'first'},
+    {id: 2 ,name: 'larry', slot: 'second'},
+    {id: 3 ,name: 'curly', slot: 'third'},
+    {id: 4 ,name: 'lucy', slot: 'third',selected:true},
+
+];
+
